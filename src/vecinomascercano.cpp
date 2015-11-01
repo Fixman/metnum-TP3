8966f14eb9d1603920be2c15dd689b0819a0f521
Martin Fixman <martinfixman@gmail.com>
2015-11-01 18:16:00 -0300

    Pequeña modificación a Vecino Más Cercano.

--- src/vecinomascercano.cpp
+++ src/vecinomascercano.cpp
@@ -1,11 +1,15 @@
 #include "vecinomascercano.h"
 
+#include <cmath>
+
 PixelTiempo VecinoMasCercano::alentarPixel(const PixelTiempo &P, int c0, int c1) const
 {
 	PixelTiempo r(c1);
 
+	double scale = (c1 - 1) / (c0 - 1);
+
 	for (int i = 0; i < r.size(); i++)
-		r[i] = P[c0 * i / c1];
+		r[i] = P[std::round(i / scale)];
 
 	return r;
 }
