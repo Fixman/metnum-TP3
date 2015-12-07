836c71a9b3d3e7ce6e973b42dd91b9f4c1f0b360
Martin Fixman <martinfixman@gmail.com>
2015-12-07 20:29:02 -0300

    Agregando nueva experimentacion.

---
+++ src/comparebaby.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+out='measureTimes/baby_reset_ecm.txt'
+echo 'Reset,Salto,PromECM' > $out
+
+for reset in 4 8 12 16 20; do
+	for jump in 2 4 6 8 10 12; do
+		echo -n "$reset,$((jump - 1))," >> $out
+		python compare.py data/baby.avi "output/baby_splines_${jump}_${reset}.avi" --method interpolacionPorSplines --jump $jump --reset $reset |
+		grep 'ECM' | grep -o '[0-9.]*' >> $out
+	done;
+done;
