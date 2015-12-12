70fd27cdbda8124aae1b3b2e692bb0e7408856a6
Martin Fixman <martinfixman@gmail.com>
2015-12-11 22:24:53 -0300

    Borre algunos scripts obsoletos.

--- src/comparebaby.sh
+++ src/comparebaby.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Corre compare.py en baby.avi con el metodo de splines y diferentes valores
+# de reset.
+
 out='measureTimes/baby_reset_ecm.txt'
-# echo 'Reset,Salto,PromECM' > $out
+echo 'Reset,Salto,PromECM' > $out
 
 for reset in 4 8 12 16 20; do
-	for jump in 11; do
+	for jump in 2 4 6 8 10 11 12; do
 		echo -n "$reset,$((jump - 1))," >> $out
 		python compare.py data/baby.avi "output/baby_splines_${jump}_${reset}.avi" --method interpolacionPorSplines --jump $jump --reset $reset |
 		grep 'ECM' | grep -o '[0-9.]*' >> $out
